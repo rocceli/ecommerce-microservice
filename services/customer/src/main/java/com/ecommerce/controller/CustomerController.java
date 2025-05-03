e07574d93e17dbd4979778fe6cae14d35a90a046
rocceli <ngugielijah08@gmail.com>
2025-05-03 20:29:56 +0300

    product service

--- services/customer/src/main/java/com/ecommerce/controller/CustomerController.java
+++ services/customer/src/main/java/com/ecommerce/controller/CustomerController.java
@@ -44,7 +44,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.existsById(customerId));
     }
 
-    @GetMapping("/{customer-id}")tat
+    @GetMapping("/{customer-id}")
     public ResponseEntity<CustomerResponse> findById(@PathVariable("customer-id") String customerId) {
 
         return ResponseEntity.ok(customerService.findById(customerId));
